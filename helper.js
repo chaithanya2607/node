e6da38a8ec76c3114bf6acaafc94414d7e07008e
chaithanya2607 <rchaithanyaraot@gmail.com>
2022-09-12 21:36:56 +0530

    added auth

--- helper.js
+++ helper.js
@@ -1,17 +1,42 @@
-import {client} from "./index.js";
+import { client } from "./index.js";
+import bcrypt from "bcrypt";
 
-export async function getAllMovies(request){
-    return await client.db("node").collection("movies").find(request.query).toArray();
+
+export async function getAllMovies(request) {
+    return await client.db("b37wd").collection("movies").find(request.query).toArray();
 }
-export async function getMovieById(id){
-    return await client.db("node").collection("movies").findOne({id:id});
+export async function getMovieById(id) {
+    return await client.db("b37wd").collection("movies").findOne({ id: id });
 }
-export async function deleteMovieById(id){
-    return await client.db("node").collection("movies").deleteOne({id:id});
+export async function deleteMovieById(id) {
+    return await client.db("b37wd").collection("movies").deleteOne({ id: id });
 }
-export async function addMovies(newMovies){
-    return await client.db("node").collection("movies").insertMany(newMovies);
+export async function addMovies(newMovies) {
+    return await client.db("b37wd").collection("movies").insertMany(newMovies);
 }
-export async function updateMovie(id,updateMovie){
-    return await client.db("node").collection("movies").updateOne({id:id},{$set:updateMovie})
+
+export async function updateMovieById(id, updateMovie) {
+    return await client.db("b37wd").collection("movies").updateOne({ id: id }, { $set: updateMovie })
+}
+
+
+
+export async function genPassword(password)
+{
+  const salt  = await bcrypt.genSalt(10); //bcrypt.genSalt(no of rounds)
+  console.log(salt)
+  const hashPassword =  await bcrypt.hash(password, salt)
+  return hashPassword
+}
+
+
+export async function createUser(username, hashedPassword) {
+    return await client.db("b37wd").collection("users")
+    .insertOne({username: username, password: hashedPassword});
+}
+
+
+export async function getUserByName(username) {
+    return await client.db("b37wd").collection("users")
+    .findOne({username: username});
 }
--- routes/movies.js
+++ routes/movies.js
@@ -1,10 +1,10 @@
 import express from "express";
-import {getAllMovies,insertMovies,deleteMoviesById,getAllMoviesById,updateMovie} from "../helper.js"
+import { getAllMovies,getMovieById, deleteMovieById, addMovies, updateMovieById } from "../helper.js"
+import { auth } from "../middleware/auth.js"
 
-const router=express.Router();
+const router = express.Router();
 
-
-
+//get all movies
 router.get("/", async (request, response) =>  {
 
     if(request.query.rating){
@@ -17,11 +17,24 @@ router.get("/", async (request, response) =>  {
 });
 
 
+//inbuild middleware - say data is in JSON
+
+//post method  - to insert data to db
+router.post("/", async (request, response) =>  {  
+    const newMovies = request.body;
+    console.log(newMovies);
+    //db.movies.insertMany(movies) 
+   const result = await addMovies(newMovies);
+   response.send(result);
+    });
+
+   
+ // send only movie with the matched id 
 router.get("/:id", async (request, response) =>  {    
     const { id } = request.params;
     console.log(id)
-
-    const movie = await getAllMoviesById(id)
+    //db.movies.findOne({id: "102"})
+    const movie = await getMovieById(id)
     
     console.log(movie)
     movie 
@@ -29,35 +42,25 @@ router.get("/:id", async (request, response) =>  {
     : response.status(404).send({ message: "No movie found" });
 });
 
-
-
 //Delete a movie id
 
 router.delete("/:id", async (request, response) =>  {    
     const { id } = request.params;
     console.log(id)
     //db.movies.deleteOne({id: "102"})
-    const movie = await deleteMoviesById(id)
+    const movie = await deleteMovieById(id)
     response.send(movie)
 });
-router.post("/", async (request, response) =>  {    
-    
-    const newMovies=request.body;
-    
-    //db.movies.findOne({id: "102"})
-    const result = await insertMovies(newMovies)
-    response.send(result)
 
+// update a movie id 
+router.put("/:id", async (request, response) =>  {    
+    const { id } = request.params;
+    const updateMovie = request.body;
+    //db.movies.updateOne({id: "102"}, { $set: updateMovie })
+    const result = await updateMovieById(id, updateMovie)
+    response.send(result)
 });
 
-router.put("/", async (request, response) =>  {    
-    const{id}=request.params;
-    const updateMovie=request.body;
-    
-    //db.movies.findOne({id: "102"})
-    const result = await updateMovie(updateMovie,id)
-    response.send(result)
 
-});
 
-export const moviesRouter=router;
+ export const moviesRouter = router;
